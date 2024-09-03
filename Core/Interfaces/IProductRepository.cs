c6f8b0b99662a18e5c2c5f31c2985259b8b71800
AntonOstap76 <antonostapchuk76@gmail.com>
2024-09-03 15:19:26 +0200

    creating repository interface and implementation class

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+    //define what are returning 
+    Task<IReadOnlyList<Product>>GetProductsAsync();
+
+    //?-optinal operator specify if method could return product or null
+    Task<Product?>GetProductByIdAsync(int id);
+
+    void AddProduct(Product product);
+
+    void UpdateProduct(Product product);
+
+    void DeleteProduct(Product product);
+
+    bool ProductExists(int id);
+
+    Task<bool>SaveChangesAsync();
+}
