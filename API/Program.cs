6edfcdadf4a8d44bdc42074d9c344ba94fac18cf
AntonOstap76 <antonostapchuk76@gmail.com>
2024-09-12 15:47:08 +0200

    Creating Generic epository

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddDbContext<StoreContext>(opt=>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+//buid a service for generic repository
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
