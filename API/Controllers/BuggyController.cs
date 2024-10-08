5ccdb7231e207b28816198fd340ce3e2d704f6fc
AntonOstap76 <antonostapchuk76@gmail.com>
2024-10-08 19:05:08 +0200

    add a test controller for error handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+using System;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Not a good request");
+    }
+
+
+    [HttpGet("notFound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("That is a new exception ");
+    }
+
+
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(Product product)
+    {
+        return Ok();
+    }
+}
