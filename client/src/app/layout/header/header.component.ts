41f6e3c616924aa3959083da8000786d58ad0dfb
AntonOstap76 <antonostapchuk76@gmail.com>
2024-12-04 18:49:29 +0100

    adding some new pictures for data. Creating new components. Setting up the links for routing.

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,8 @@ import {MatIcon} from '@angular/material/icon';
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
+    MatBadge,
+    RouterLink,RouterLinkActive //routerLInkActive to specify a class that is going to be applied when route is active 
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
