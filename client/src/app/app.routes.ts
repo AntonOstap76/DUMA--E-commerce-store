16d3832ed844fa8657fef7da40aa76fe4851381b
AntonOstap76 <antonostapchuk76@gmail.com>
2024-12-06 21:05:43 +0100

    create http interceptor

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
 import { TestErrorComponent } from './features/test-error/test-error.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
@@ -10,5 +12,7 @@ export const routes: Routes = [
     //for opening product details with specified id 
     {path:'shop/:id', component:ProductDetailsComponent},
     {path:'test-error', component:TestErrorComponent},
-    {path:'**', redirectTo:'', pathMatch:'full'}
+    {path:'not-found', component:NotFoundComponent},
+    {path:'server-error', component:ServerErrorComponent},
+    {path:'**', redirectTo:'not-found', pathMatch:'full'}// if request is uncorrect or doesnt exist in redirect to fot found error
 ];
