92de4eb4990398bfab5008caf5935c76a72c94f1
AntonOstap76 <antonostapchuk76@gmail.com>
2025-02-21 13:46:17 +0100

    create order aggregate and configure order enteties

---
+++ Infrastructure/Config/OrderConfiguration.cs
@@ -0,0 +1,31 @@
+using System;
+using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Config;
+
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Order> builder)
+    {
+        // order own this entities
+        builder.OwnsOne(x=>x.ShippingAddress, o=>o.WithOwner());
+        builder.OwnsOne(x=>x.PaymentSummary, o=>o.WithOwner());
+
+        //for enum
+        //to get enum value rather than index of the item in enum list
+        builder.Property(x=>x.Status).HasConversion(o=>o.ToString(),
+                                                     o=>(OrderStatus)Enum.Parse(typeof(OrderStatus),o));
+
+        builder.Property(x=>x.Subtotal).HasColumnType("decimal(18,2)");
+
+        //one to many relationship
+        builder.HasMany(x=>x.OrderItems).WithOne().OnDelete(DeleteBehavior.Cascade);
+
+        //convert to utc 
+        builder.Property(x=>x.OrderDate).HasConversion(d=>d.ToUniversalTime(),
+                                                        d=>DateTime.SpecifyKind(d,DateTimeKind.Utc));
+        
+    }
+}
