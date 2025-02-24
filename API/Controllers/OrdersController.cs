fc20ae505e4a6f0d299130936232f897d0e2fdd1
AntonOstap76 <antonostapchuk76@gmail.com>
2025-02-24 12:19:40 +0100

    shape data to return

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -77,18 +77,20 @@ public class OrdersController(ICartService cartService, IUnitOfWork unitOfWork) 
     }
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+    public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
     {
         var spec = new OrderSpecification(User.GetEmail());
 
         var orders = await unitOfWork.Repository<Order>().ListAsync(spec);
 
-        return Ok(orders);
+        var ordersToReturn = orders.Select(o=>o.ToDto()).ToList();
+
+        return Ok(ordersToReturn);
 
     }
 
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<Order?>> GetOrderById(int id)
+    public async Task<ActionResult<OrderDto>> GetOrderById(int id)
     {
         var spec = new OrderSpecification(User.GetEmail(), id);
 
@@ -96,6 +98,6 @@ public class OrdersController(ICartService cartService, IUnitOfWork unitOfWork) 
 
         if(order == null) NotFound();
 
-        return order;
+        return order.ToDto();
     }
 }
